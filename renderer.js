0cf708ac8247e84eb86e6229b7ea189615ecef8d
mert <57325062+valtair0@users.noreply.github.com>
2023-05-13 23:20:12 +0300

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,11 +1,29 @@
-const information = document.getElementById("info");
+const actualText = document.getElementById("actualText");
+const shadowText = document.getElementById("shadowText");
+const start = document.getElementById("start");
+let timeoutId;
 
 window.version.handleCopy((event, value) => {
   console.log("handleCounter", value);
   if (value == "") {
-    //pc ilk açıldığında clipboard boş olduğu için undefined döndürüyor
-    information.innerText = "Clipboard is empty";
+    actualText.innerText = "Clipboard is empty";
+    shadowText.innerText = "Clipboard is empty";
   } else {
-    information.innerText = value;
+    actualText.classList.remove("hidden");
+    shadowText.classList.remove("hidden");
+
+    actualText.innerText = value;
+    shadowText.innerText = value;
+
+    clearTimeout(timeoutId); // Clear the previous timeout
+
+    timeoutId = setTimeout(() => {
+      actualText.classList.add("hidden");
+      shadowText.classList.add("hidden");
+    }, 10000); // Hide after 10 seconds
   }
+});
+
+start.addEventListener("click", () => {
+  window.version.openText(screen.width);
 });
